4139e57269f93593f12ebf15aebcf43a7fbf9ada
Suman Gajmer <suman.gajmer@f1soft.com>
2023-05-13 00:32:36 +0545

    fixed test case failure issue

--- src/test/java/com/musala/drones/mock/LoadDroneMock.java
+++ src/test/java/com/musala/drones/mock/LoadDroneMock.java
@@ -2,10 +2,15 @@ package com.musala.drones.mock;
 
 import com.musala.drones.model.LoadDroneModel;
 import com.musala.drones.model.MedicationItemModel;
+import lombok.extern.slf4j.Slf4j;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
+@Slf4j
 public class LoadDroneMock {
 
     public static LoadDroneModel getLoadDrone(String serialNumber, Integer itemSize, Double weight) {
@@ -21,9 +26,22 @@ public class LoadDroneMock {
             MedicationItemModel medicationItemModel = new MedicationItemModel();
             medicationItemModel.setName("Metformin");
             medicationItemModel.setWeight(weight);
+            medicationItemModel.setImage(getImageAsByteString());
             medicationItemModels.add(medicationItemModel);
         }
         return medicationItemModels;
+    }
+
+    private static String getImageAsByteString() {
+        try {
+            byte[] imageBytes = Files.readAllBytes(Paths.get("medication.jpg"));
+            String base64String = Base64.getEncoder().encodeToString(imageBytes);
+            return base64String;
+        } catch (Exception e) {
+            log.error("error converting image to byte string : {} ", e.getMessage());
+            return "";
+        }
+
     }
 
 }
